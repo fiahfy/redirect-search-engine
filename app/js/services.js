71e23a948ce198d0f75eea898de72017b02c2857
fiahfy <d.fiahfy@gmail.com>
2014-09-29 18:04:37 +0900

    update

--- app/js/services.js
+++ app/js/services.js
@@ -7,22 +7,22 @@ var services = angular.module('services', []);
 services.service('RedirectService', function() {
   this.setup = function(){
     chrome.webRequest.onBeforeRequest.addListener(
-      function(details) {
-        var parser = document.createElement('a');
-        parser.href = details.url;
+        function(details) {
+          var parser = document.createElement('a');
+          parser.href = details.url;
 
-        var q = '';
-        var match = parser.search.match(/[?&]p=(.*?)(&|$)/i);
-        if (match) {
-          q = match[1];
-        }
+          var q = '';
+          var match = parser.search.match(/[?&]p=(.*?)(&|$)/i);
+          if (match) {
+            q = match[1];
+          }
 
-        return {
-          redirectUrl: 'https://www.google.com/#q=' + q
-        };
-      },
-      {urls: ["*://*/search*"]},
-      ["blocking"]
+          return {
+            redirectUrl: 'https://www.google.com/#q=' + q
+          };
+        },
+        {urls: ["*://*/search*"]},
+        ["blocking"]
     );
   };
 });
